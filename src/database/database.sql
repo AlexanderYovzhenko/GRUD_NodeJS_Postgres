d556a0d8ae74a845a607308a5c147bf6c927669f
Alexander <renyreny125tt@gmail.com>
2023-03-13 19:55:46 +0300

    refactor: change schema db on many-to-many

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,21 +1,32 @@
+-- Creation of a database for storing films and film genres
 CREATE DATABASE node_postgres;
 
+
+-- Create tables
 CREATE TABLE movie
 (
-  movie_id SERIAL UNIQUE PRIMARY KEY,
-  movie_name VARCHAR(64) NOT NULL,
-  year SMALLINT NOT NULL
+  movie_id serial PRIMARY KEY,
+  movie_name varchar(64) NOT NULL,
+  year smallint NOT NULL
 );
 
 CREATE TABLE genre 
 (
-  genre_id SERIAL UNIQUE PRIMARY KEY,
-  genre_name VARCHAR(64) NOT NULL,
-  movie_id SMALLINT NOT NULL,
-  FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
+  genre_id serial PRIMARY KEY,
+  genre_name varchar(64) NOT NULL
+);
+
+CREATE TABLE movie_genre
+(
+  movie_genre_id serial PRIMARY KEY,
+  movie_id int NOT NULL,
+  genre_id int NOT NUll,
+  FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+  FOREIGN KEY (genre_id) REFERENCES genre(genre_id) ON DELETE CASCADE
 );
 
 
+-- Insert tables
 INSERT INTO movie(movie_name, year)
 VALUES 
 ('Destroyer', 2018),
@@ -24,17 +35,30 @@ VALUES
 ('Brad''s Status', 2017),
 ('Rock of Ages', 2012);
 
-INSERT INTO genre(genre_name, movie_id)
+INSERT INTO genre(genre_name)
 VALUES
-('thriller', 1),
-('comedy', 2),
-('action', 2),
-('action', 3),
-('comedy', 3),
-('thriller', 3),
-('comedy', 4),
-('drama', 4),
-('music', 4),
-('music', 5),
-('drama', 5),
-('comedy', 5);
+('thriller'),
+('comedy'),
+('action'),
+('drama'),
+('music');
+
+INSERT INTO movie_genre(movie_id, genre_id) 
+VALUES
+(1, 1),
+(2, 2),
+(2, 3),
+(3, 2),
+(3, 3),
+(3, 1),
+(4, 2),
+(4, 4),
+(4, 5),
+(5, 5),
+(5, 4);
+
+
+-- Delete tables
+DROP TABLE movie_genre;
+DROP TABLE genre;
+DROP TABLE movie;
